4b5482757e9f3017e80d7ffdd7c71dff5f8e708f
Nguyen <tranchauthanhnguyen@gmail.com>
2021-10-07 00:01:52 +0700

    upload

---
+++ build/precache-manifest.2e95c9aaf063b14a88876cbe444b3712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0daa9675960b5588f44c79edc0c19cd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfe08b89d5495500aca1",
+    "url": "/static/css/main.791e838a.chunk.css"
+  },
+  {
+    "revision": "d27d1ee370e0bb7c1474",
+    "url": "/static/js/2.ec23ad99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ec23ad99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe08b89d5495500aca1",
+    "url": "/static/js/main.e89137fa.chunk.js"
+  },
+  {
+    "revision": "16b33f2b0eb07435af46",
+    "url": "/static/js/runtime-main.89d0faaf.js"
+  }
+]);
